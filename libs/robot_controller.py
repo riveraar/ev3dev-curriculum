642ce4a2dad8e04f04f80de2e5ca89d17c442cc4
riveraar <riveraar@rose-hulman.edu>
2018-01-31 20:38:30 -0500

    Merge branch 'master' of C:\Users\riveraar\Documents\Rose\CSSE120\PyCharm\ev3dev-curriculum with conflicts.

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -22,4 +22,3 @@ class Snatch3r(object):
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
     def __init__(self):
-
--- sandbox/dougheea/motors/m2_drive_encoders.py
+++ sandbox/dougheea/motors/m2_drive_encoders.py
@@ -7,16 +7,13 @@ You will now use a run_to_rel_pos command to implement the action drive inches a
 Authors: David Fisher and Emily Dougherty.
 """  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# Done: 2. Copy the contents of your m1_drive_timed.py and paste that text into
-#  this file below these comments.
+# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
-# Done: 3. Add a beep after the drive motors stop (see code below).  Test your
-# code to hear the beep AFTER movement.
+# TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
-# Done: 4. Instead of using the run_forever, time.sleep, stop pattern switch to
-#  using the run_to_rel_pos command.
+# TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
 #   You will need to determine the position_sp value to pass into the run_to_rel_pos command as a named argument.
 #   Assume the diameter of the wheel is 1.3" (close enough).  A 1.3" diameter wheel results in approximately a 4"
 #     circumference, so 360 degrees = 4 inches of travel.
@@ -32,11 +29,9 @@ Authors: David Fisher and Emily Dougherty.
 #        -- speed_sp
 #        -- stop_action
 
-# Done: 5. Make sure the beep happens AFTER the motors stop.  Use the
-# wait_while command to block code execution.
+# TODO: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
 
-# Done: 6. Formally test your work. When you think you have the problem
-# complete run these tests:
+# TODO: 6. Formally test your work. When you think you have the problem complete run these tests:
 #   200 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 2 inches of the target distance)
@@ -45,8 +40,7 @@ Authors: David Fisher and Emily Dougherty.
 #   400 dps -36 inches (make sure it drives within 3 inches of the target distance)
 # Add more tests as you see fit.  Ideally you should be +/- 10% of the target goal this time.
 
-# Done: 7. Call over a TA or instructor to sign your team's checkoff sheet and
-# do a code review.
+# TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
 
@@ -72,17 +66,14 @@ def main():
     while time_s != 0:
         speeds_sp = int(input("Enter a speed (0 to 900 dps): "))
         distance_sp = int(input("Distance to travel (inches): "))
-        if speeds_sp == 0 or distance_sp == 0:
-            break
 
-        left_motor.run_to_rel_pos(position_sp = distance_sp * 90, speed_sp =
-        speeds_sp, stop_action="brake")
-        right_motor.run_to_rel_pos(position_sp = distance_sp * 90, speed_sp =
-        speeds_sp, stop_action="brake")
-        left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        right_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        ev3.Sound.beep().wait()
+        left_motor.run_forever(speed_sp=speeds_sp)
+        right_motor.run_forever(speed_sp=speeds_sp)
+        time.sleep(distance_sp / 3.33)
+        left_motor.stop()
+        right_motor.stop(stop_action="brake")
 
+    ev3.Sound.beep().wait()
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
 
